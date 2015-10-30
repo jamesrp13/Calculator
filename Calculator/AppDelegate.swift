9246b1872aba38268f821ffef9050b0a78d516cf
“pkadams67” <paul.k.adams@me.com>
2015-10-30 14:13:46 -0600

    Remove Storyboard

--- Calculator/AppDelegate.swift
+++ Calculator/AppDelegate.swift
@@ -13,9 +13,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-    
+
+        window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        window?.backgroundColor = .redColor()
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+
         return true
     }
 
